9e8fd8ddfee3edbbe8bd46be0d2b05a1d0a90f7d
sergi <sergio.ume@hotmail.com>
2023-07-30 11:35:04 +0100

    RELOJ Y ARREGLOS DE BOX Y SIDEBAR PARA QUE ESTEN A LA MISMA ALTURA

--- js/main.js
+++ js/main.js
@@ -1,104 +1,66 @@
 $(document).ready(function() {
-   // Slider
-   $('.bxslider').bxSlider({
-     mode: 'fade',
-     captions: true,
-     slideWidth: 1200
-   });
-
-   // Post
-   var posts = [
-     {
-       title: 'Prueba de titulo 1',
-       date:'Publicado el día '+ moment().date()+ " de "+ moment().format("MMMM") + " del "+moment().format("YYYY"),//plugin de MOMENT
-       content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas iaculis purus ac nunc bibendum scelerisque. Fusce consectetur mi a lorem feugiat, sed laoreet diam placerat.'
-     },
-     {
-       title: 'Prueba de titulo 2',
-       date:'Publicado el día '+ moment().date()+ " de "+ moment().format("MMMM") + " del "+moment().format("YYYY"),//plugin de MOMENT
-       content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas iaculis purus ac nunc bibendum scelerisque. Fusce consectetur mi a lorem feugiat, sed laoreet diam placerat.'
-     },
-     {
-       title: 'Prueba de titulo 3',
-       date:'Publicado el día '+ moment().date()+ " de "+ moment().format("MMMM") + " del "+moment().format("YYYY"),//plugin de MOMENT
-       content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas iaculis purus ac nunc bibendum scelerisque. Fusce consectetur mi a lorem feugiat, sed laoreet diam placerat.'
-     },
-     {
-       title: 'Prueba de titulo 4',
-       date:'Publicado el día '+ moment().date()+ " de "+ moment().format("MMMM") + " del "+moment().format("YYYY"),//plugin de MOMENT
-       content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas iaculis purus ac nunc bibendum scelerisque. Fusce consectetur mi a lorem feugiat, sed laoreet diam placerat.'
-     },
-     {
-       title: 'Prueba de titulo 5',
-       date:'Publicado el día '+ moment().date()+ " de "+ moment().format("MMMM") + " del "+moment().format("YYYY"),//plugin de MOMENT
-       content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas iaculis purus ac nunc bibendum scelerisque. Fusce consectetur mi a lorem feugiat, sed laoreet diam placerat.'
-     },
-   ];
-
- posts.forEach((item, index) => {
-   var post = `
-     <article class="post">
-       <h2>${item.title}</h2>
-       <span class="date">${item.date}</span>
-       <p>
-         ${item.content}
-       </p>
-       <a href="#" class="button-more">Leer más</a>
-     </article>
-   `;
-    $("#posts").append(post); //PARA CARGAR EN INDEX EL POST
-
- });
-
-
-   /*ESTO ERA EL ESTADO INICIAL ANTES DE UTILIZAR JSON Y JQUERY
-   <article class="post">
-                       <h2>Prueba de titulo</h2>
-                       <span class="date">Fecha de publicación</span>
-                       <p>
-                           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                           Maecenas iaculis purus ac nunc bibendum scelerisque.
-                           Fusce consectetur mi a lorem feugiat, sed laoreet diam placerat.
-                           Curabitur dignissim ut leo eu vestibulum. In ac commodo nunc, quis vulputate tellus.
-                           Duis nec consequat elit, sed vehicula odio. Integer dapibus urna eu justo cursus,
-                           vel facilisis risus faucibus.
-                           Aliquam pharetra placerat elit, quis consequat dui posuere quis.
-                           Praesent interdum nisi id velit aliquam rutrum. Quisque quis fringilla augue.
-                           Vivamus non aliquet massa. Aliquam erat volutpat. Vivamus consectetur,
-                           nisi vel varius facilisis,
-                           turpis sem euismod magna, id auctor purus arcu in risus.
-                           Praesent vestibulum vitae risus semper tincidunt.
-                           Suspendisse vulputate nisi eros, ut tincidunt nisl fermentum eu.
-                       </p>
-                       <a href="#" class="button-more">Leer más</a>
-                   </article>*/
-
-   //CAPTURANDO EL EVENTO CLICK PARA CAMBIAR DE COLOR
-   var theme =$("#theme");
-   $("#to-green").click(function(){
-     theme.attr("href","css/green.css")
-   });
-   $("#to-red").click(function(){
-     theme.attr("href","css/red.css")
-   });
-   $("#to-blue").click(function(){
-     theme.attr("href","css/blue.css")
+  // Slider
+  if (window.location.href.indexOf('index') > -1) {
+    $('.bxslider').bxSlider({
+      mode: 'fade',
+      captions: true,
+      slideWidth: 1200
     });
+  }
 
-    //EVENTO PARA PULSAR I SUBIR ARRIBA
+  // Post
+  if (window.location.href.indexOf('index') > -1) {
+    var posts = [
+      {
+        title: 'Prueba de titulo 1',
+        date: 'Publicado el día ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas iaculis purus ac nunc bibendum scelerisque. Fusce consectetur mi a lorem feugiat, sed laoreet diam placerat.'
+      },
+      {
+        title: 'Prueba de titulo 2',
+        date: 'Publicado el día ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas iaculis purus ac nunc bibendum scelerisque. Fusce consectetur mi a lorem feugiat, sed laoreet diam placerat.'
+      },
+      // Agrega aquí más objetos de posts si es necesario
+    ];
 
-    $('.subir').click(function(e){
+    posts.forEach((item, index) => {
+      var post = `
+        <article class="post">
+          <h2>${item.title}</h2>
+          <span class="date">${item.date}</span>
+          <p>
+            ${item.content}
+          </p>
+          <a href="#" class="button-more">Leer más</a>
+        </article>
+      `;
+      $("#posts").append(post); // Añadir el post al elemento con id "posts"
+    });
+  }
+
+  // Cambiar el tema de color
+  var theme = $("#theme");
+  $("#to-green").click(function() {
+    theme.attr("href", "css/green.css");
+  });
+  $("#to-red").click(function() {
+    theme.attr("href", "css/red.css");
+  });
+  $("#to-blue").click(function() {
+    theme.attr("href", "css/blue.css");
+  });
+
+  // Evento para pulsar y subir arriba
+  $('.subir').click(function(e) {
     e.preventDefault();
     $('html,body').animate({
-            scrollTop: 0 //metodo jquery pixel 0 desde donde empieza la web
-        }, 500);
-        return false;
-    });
+      scrollTop: 0
+    }, 500);
+    return false;
+  });
 
-
-//FALSO LOGIN
-
-$(document).ready(function() {
+  // FALSO LOGIN
   $("#login form").submit(function() {
     var form_name = $("#form_name").val();
     localStorage.setItem("form_name", form_name);
@@ -119,8 +81,23 @@ $(document).ready(function() {
       location.reload(); // Recargar la página para volver al formulario de inicio de sesión
     });
   }
-});
+//ACORDEON
+  if (window.location.href.indexOf('about') > -1) {
+     $("#acordeon").accordion({
+       collapsible: true, // Permite cerrar todos los paneles abiertos
+       active: false, // Ningún panel abierto por defecto
+       heightStyle: "content" // Ajusta automáticamente la altura del panel al contenido
+     });
+   }
 
+   if(window.location.href.indexOf('reloj') > -1){
+
+   setInterval(function(){ //REPITE EL INTERCALO CADA 1000ms PARA QUE CORRAN LOS SEGUNDOS
+        var reloj = moment().format("hh:mm:ss"); //METODOS DE MOMENT.JS
+        $('#reloj').html(reloj);
+   },1000);
+
+   }
 
 
  });
